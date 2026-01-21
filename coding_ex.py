81f2ee833107f668e1b8ec15e93f5c086e6cb097
Keed728 <keerthana728driya@gmail.com>
2026-01-21 19:00:38 +0530

    initial commit

---
+++ coding_ex.py
@@ -0,0 +1,14 @@
+"""
+when all sides of the triangle is known
+semi perimeter (s)=(a+b+c)/2
+area = square root of (s*(s-a)*(s-b)*(s-c))
+
+"""
+
+a=float(input("enter first side of triangle:"))
+b=float(input("enter second side of triangle:"))
+c=float(input("enter third side of triangle:"))
+s=(a+b+c)/2
+area=(s*(s-a)*(s-b)*(s-c))**0.5
+print("the area of the triangle is ",area)
+
---
+++ welcome.py
@@ -0,0 +1,22 @@
+import keyword
+a=100.27
+print(type(a))
+
+print(int(a))
+
+
+h='100'
+print(type(h))
+
+print(int(h))
+
+lan="python"
+ver=3.13
+#print(lan+ver)
+print(lan + ' ' + str(ver))
+
+print('100'+'100')
+print(100+100)
+
+
+
